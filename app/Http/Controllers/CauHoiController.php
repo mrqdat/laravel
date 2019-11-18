928bb15851b02e7c15f78ece8518904a6a6b8f2a
heyday <54289320+heyday1515@users.noreply.github.com>
2019-11-18 22:55:35 +0700

    comment lĩnh vực & câu hỏi

--- app/Http/Controllers/CauHoiController.php
+++ app/Http/Controllers/CauHoiController.php
@@ -17,15 +17,18 @@ class CauHoiController extends Controller
             'noidung' => $listCauHoi
         ]);
     }
-    public function LayLinhVuc(){
-        $listLinhVuc = Linh_Vuc::all();
-        return view('ThemCH',['tenLinhVuc' => $listLinhVuc]);
-    }
+
+    //chuyển hướng qua form thêm câu hỏi
     public function FormThemCauHoi(){
+
+        //lấy lĩnh vực để hiển thị lên Drop-down
         $listLinhVuc = Linh_Vuc::all();
         return view('ThemCH',['tenLinhVuc' => $listLinhVuc]);
     }
+    //Thêm mới câu hỏi
     public function ThemDataCauHoi(Request $req){
+
+        //Lấy dữ liệu từ input
         $noidung = $req->Noidung;
         $dapan = $req->Dapan;
         $idlv = $req->IdLV;
@@ -35,6 +38,7 @@ class CauHoiController extends Controller
         $pc = $req->PaC;
         $pd = $req->PaD;
         
+        //Tạo câu hỏi
         cau_hoi::create(
             ['noi_dung'=>$noidung,
             'dap_an'=>$dapan,
@@ -51,9 +55,13 @@ class CauHoiController extends Controller
         
     }
 
+    //Hiển thị câu hỏi vừa chọn lên form sửa câu hỏi
     public function TimCauHoi($id)
     {
+        //lấy lĩnh vực để hiển thị lên Drop-down
         $listLinhVuc = Linh_Vuc::all();
+
+        //Lấy câu hỏi
         $cauhoi = cau_hoi::find($id);
         return view('SuaCauHoi',
             ['cauhoifind'=>$cauhoi,
@@ -61,13 +69,16 @@ class CauHoiController extends Controller
             ]);
     }
 
+    //Cập nhật thông tin vừa sửa
     public function CapNhatCauHoi($id, Request $req){
+        //tìm câu hỏi
         $cauhoi = cau_hoi::find($id);
 
         if($cauhoi == null){
             self::error('Update failed');
         }
         else{
+            //cập nhật câu hỏi
             $cauhoi->update(
                 ['noi_dung'=>$req->Noidung,
                 'dap_an'=>$req->Dapan,
@@ -78,12 +89,13 @@ class CauHoiController extends Controller
                 'phuong_an_C'=>$req->PaC,
                 'phuong_an_D'=>$req->PaD
             ]);
-
+            
             self::success('Update success');
             return redirect()->route('CauHoiRoute');
         }
     }
     
+    //Xóa câu hỏi vừa chọn
     public function XoaDataCauHoi($id){
         $cauhoi = cau_hoi::find($id);
         
--- app/Http/Controllers/LinhVucController.php
+++ app/Http/Controllers/LinhVucController.php
@@ -16,19 +16,26 @@ class LinhVucController extends Controller
     }
 
     public function ThemDataLinhVuc(Request $req){
+
+        //lấy dữ liệu từ input
         $tenlv = $req->TenLV;
 
+        //thêm mới lĩnh vực
         Linh_Vuc::create(['ten_linh_vuc'=>$tenlv]);
-
         return redirect()->route('LinhVucRoute');
 
     }
 
     public function TimLinhVuc($id){
+
+        //lấy lĩnh vực để hiển thị lên Drop-down
         $linhvuc = Linh_Vuc::find($id);
+
+        //truyền biến $linhvuc qua SuaLinhVuc.blade.php với tên 'linhvucfind'
         return view('SuaLinhVuc',['linhvucfind'=>$linhvuc]);
     }
 
+    //Cập nhật lĩnh vực
     public function CapNhatLinhVuc($id, Request $req){
         $linhvuc = Linh_Vuc::find($id);
 
