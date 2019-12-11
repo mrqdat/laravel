4c375a79474741c88ca72c326e7de13382f0f538
heyday <54289320+heyday1515@users.noreply.github.com>
2019-12-11 11:36:07 +0700

    API câu hỏi

--- app/Http/Controllers/API/CauhoiController.php
+++ app/Http/Controllers/API/CauhoiController.php
@@ -2,4 +2,20 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\cau_hoi;
+use App\Linh_Vuc;
 
+class CauhoiController extends Controller
+{
+    public function LayCauHoi(Request $req){
+        $linhvuc = $req->query('linh_vuc');
+        $cauhoi = cau_hoi::where('linh_vuc_id', $linhvuc,)->get();
+        $result = [
+            'success'   =>true,
+            'data'      =>$cauhoi
+        ];
+        return response()->json($cauhoi);
+    }
+}
