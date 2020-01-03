f3485536d535055e6d94cdc30fe53168f977f254
heyday <hungthinh.ckc@gmail.com>
2020-01-03 15:17:50 +0700

    DangKy

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 
 });
 
@@ -99,7 +99,7 @@ Route::get('LinhVuc', 'LinhVucController@LayDataLinhVuc')->name('LinhVucRoute');
    Route::get('KhoiPhucLinhVuc/{id}', 'LinhVucController@KhoiPhuc')->name('KhoiPhucLinhVucRoute');
 
    //Xóa lĩnh vực khỏi thùng rác
-   Route::get('Xoa/{id}', 'LinhVucController@Xoa')->name('XoaLuonLinhVucRoute');
+  Route::get('XoaLuonLinhVuc/{id}','LinhVucController@Xoa')->name('XoaLuonLinhVucRoute');
 
 
 
